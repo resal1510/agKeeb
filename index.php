804c7c7a6af0b70dc9d486f1e8fb42c3895e7c58
Allan R <resin.allan8@gmail.com>
2020-01-24 08:22:57 +0100

    Include the header w/ php

--- index.php
+++ index.php
@@ -15,28 +15,9 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-<head>
-  <!-- Set charset, title -->
-  <meta charset="utf-8">
-  <title>agKeeb Shop</title>
-  <!-- Import Bootstrap, jQuery, PopperJS and FontAwesome -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  <script src="https://kit.fontawesome.com/a77b3e076e.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <!-- Import local JS and CSS files -->
-  <script src="/js/script.js" charset="utf-8"></script>
-  <link rel="stylesheet" href="/css/style.css">
-</head>
-<script>
-  $( document ).ready(function() {
-    console.log("Document ready");
-    $("#menuTop").load("menu.html");
-});
-</script>
+
 <body>
-  <div id="menuTop"></div>
+  <?php include "header.php"?>
 
   <div class="spacer"></div>
   <p>Debug buttons (Login) :</p>
@@ -47,8 +28,9 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
   <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/testlogin.php';">check login</button>
   <div class="spacer"></div>
   <p>Debug buttons (Admin panel things) :</p>
-  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/manageitems.php';">Enable/disable/list items</button>
-  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/manageaccounts.php';">Enable/Disable/list accounts </button>
+  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/manageitems.php';">Manage items</button>
+  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/manageaccounts.php';">Manage accounts </button>
+  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/manageorders.php';">Manage orders </button>
 </body>
 
 </html>
