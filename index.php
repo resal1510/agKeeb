68186c51ca779d18da8fec54b39e0ef0fe7a40d2
Allan R <resin.allan8@gmail.com>
2020-02-06 15:48:35 +0100

    Included navbarInclude.php

--- index.php
+++ index.php
@@ -13,24 +13,33 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 */
 ?>
 
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+?>
+
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 
 <body>
-  <?php include "header.php"?>
+  <?php include "navbarInclude.php"?>
 
   <div class="spacer"></div>
   <p>Debug buttons (Login) :</p>
-  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/index.php';">index</button>
-  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/register.php';">register</button>
-  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/login.php';">login</button>
-  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/logout.php';">logout</button>
-  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/testlogin.php';">check login</button>
+  <button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/index.php';">index</button>
+  <button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/register.php';">register</button>
+  <button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/login.php';">login</button>
+  <button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/logout.php';">logout</button>
+  <button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/testlogin.php';">check login</button>
   <div class="spacer"></div>
   <p>Debug buttons (Admin panel things) :</p>
-  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/manageitems.php';">Manage items</button>
-  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/manageaccounts.php';">Manage accounts </button>
-  <button onclick="window.location.href = 'https://agkeeb.allanresin.ch/manageorders.php';">Manage orders </button>
+  <button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/manageitems.php';">Manage items</button>
+  <button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/manageaccounts.php';">Manage accounts </button>
+  <button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/manageorders.php';">Manage orders </button>
+  <div class="spacer"></div>
+  <p>Debug buttons (normal things) :</p>
+  <button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/createorder.php';">Create an order</button>
 </body>
 
 </html>
