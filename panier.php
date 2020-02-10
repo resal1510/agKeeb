8c9f16692ebc8bc38b0bdcea8cff5317dce0ccd4
Allan R <resin.allan8@gmail.com>
2020-02-10 14:16:41 +0100

    Created base file for the shopping cart

---
+++ panier.php
@@ -0,0 +1,16 @@
+<?php
+
+ ?>
+ <?php
+ error_reporting(E_ALL);
+ ini_set('display_errors', TRUE);
+ ini_set('display_startup_errors', TRUE);
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="fr" dir="ltr">
+
+ <body>
+   <?php include "navbarInclude.php"?>
+
+   <div class="spacer"></div>
