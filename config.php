852040ca1f130aacfc968ce6c8cedba09b40c254
Allan R <resin.allan8@gmail.com>
2020-03-06 14:53:18 +0100

    Force UTF-8 into the PDO config

--- config.php
+++ config.php
@@ -7,7 +7,8 @@ define('DB_NAME', 'pqqa_agkeeb');
 
 // Connection test to see if it's working fine
 try{
-    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $pdo->exec('SET NAMES utf8');
     // Configure PDO to let him display errors (Only for dev time)
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
