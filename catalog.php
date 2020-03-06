9d70a2cec4133469c382c6111f3b28b021c02f4f
Allan R <resin.allan8@gmail.com>
2020-03-06 14:52:55 +0100

    Finish the catalog page script

--- catalog.php
+++ catalog.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+session_start ();
 
 $whatCategory = "";
 $categoryListNumbers = ["Tous les articles", "Plaques", "Circuits imprimés", "Interrupteurs", "Touches", "Câbles", "Stabilisateurs", "Boîtiers"];
@@ -11,7 +9,6 @@ if (isset($_GET["cat"])) {
 } else {
   $whatCategory = 0;
 }
-
  ?>
 
 <!DOCTYPE html>
@@ -38,11 +35,45 @@ if (isset($_GET["cat"])) {
                  10px 5px 5px rgba(0,0,0,0.05),
                  -10px 5px 5px rgba(0,0,0,0.05);"><?php echo $categoryListNumbers[$whatCategory]; ?></h2>
               </div>
+              <?php
+              if (isset($_GET['action'])) {
+                $action = $_GET['action'];
+              } else {
+                $action = "";
+              }
+
+              echo "<div class='col-md-12' style='margin-top: 72px;'>";
+                  if($action=='added'){
+                      echo "<div class='alert alert-info'>";
+                          echo "Product was added to your cart!";
+                      echo "</div>";
+                  }
+
+                  if($action=='exists'){
+                      echo "<div class='alert alert-info'>";
+                          echo "Product already exists in your cart!";
+                      echo "</div>";
+                  }
+              echo "</div>";
+               ?>
                 <div class="content">
                     <div class="row">
-                        <div class="col-md-3" style="margin-left: 155p;">
-                            <div class="d-none d-md-block text-center"><select class="mt-3"><optgroup label="This is a group"><option value="12" selected="">This is item 1</option><option value="13">This is item 2</option><option value="14">This is item 3</option></optgroup></select></div>
-                        </div>
+                      <div class="col-md-3" style="padding-right: 25px;">
+                          <div class="d-none d-md-block">
+                              <div class="filters">
+                                  <div class="filter-item">
+                                      <h3>Filtrer par:</h3><select class="custom-select"><option value="12" selected>This is item 1</option><option value="13">This is item 2</option><option value="14">This is item 3</option></select></div>
+                              </div>
+                          </div>
+                          <div class="d-md-none"><a class="btn border rounded d-md-none filter-collapse" data-toggle="collapse" aria-expanded="true" aria-controls="filters" href="#filters" role="button">Filtres<i class="icon-arrow-down filter-caret"></i></a>
+                              <div class="collapse show" id="filters">
+                                  <div class="filters">
+                                      <div class="filter-item">
+                                          <h3>Filtrer par:</h3><select class="custom-select"><option value="12" selected>This is item 1</option><option value="13">This is item 2</option><option value="14">This is item 3</option></select></div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
                         <div class="col-md-9">
                             <div class="products">
                                 <div class="row no-gutters">
@@ -64,8 +95,8 @@ if (isset($_GET["cat"])) {
                                   foreach ($result3 as $key) {
                                     $baseCode = '<div class="col-12 col-md-6 col-lg-4">
                                         <div class="clean-product-item">
-                                            <div class="image"><a href="#"><img class="img-fluid d-block mx-auto" style="height:160px" src="uploads/'.$key[$nom_image].'"></a></div>
-                                            <div class="product-name"><a href="#">'.$key[$nom].'</a></div>
+                                            <div class="image"><a href="productPage.php?id_product='.$key[$id].'"><img class="img-fluid d-block mx-auto" style="height:160px" src="uploads/'.$key[$nom_image].'"></a></div>
+                                            <div class="product-name"><a href="productPage.php?id_product='.$key[$id].'">'.$key[$nom].'</a></div>
                                             <div class="about">
                                                 <div class="rating"><img src="img/star.svg"><img src="img/star.svg"><img src="img/star.svg"><img src="img/star-half-empty.svg"><img src="img/star-empty.svg"></div>
                                                 <div class="price">
@@ -75,45 +106,47 @@ if (isset($_GET["cat"])) {
                                         </div>
                                     </div>';
 
-                                    switch ($whatCategory) {
-                                      case "1":
-                                        if ($key[$category] == "1") {
-                                          print_r($baseCode);
-                                        }
-                                        break;
-                                        case '2':
-                                        if ($key[$category] == "2") {
-                                          print_r($baseCode);
-                                        }
+                                    if ($key[$enabled] == "true") {
+                                      switch ($whatCategory) {
+                                        case "1":
+                                          if ($key[$category] == "1") {
+                                            print_r($baseCode);
+                                          }
                                           break;
-                                          case '3':
-                                          if ($key[$category] == "3") {
+                                          case '2':
+                                          if ($key[$category] == "2") {
                                             print_r($baseCode);
                                           }
                                             break;
-                                            case '4':
-                                            if ($key[$category] == "4") {
+                                            case '3':
+                                            if ($key[$category] == "3") {
                                               print_r($baseCode);
                                             }
                                               break;
-                                              case '5':
-                                              if ($key[$category] == "5") {
+                                              case '4':
+                                              if ($key[$category] == "4") {
                                                 print_r($baseCode);
                                               }
                                                 break;
-                                                case '6':
-                                                if ($key[$category] == "6") {
+                                                case '5':
+                                                if ($key[$category] == "5") {
                                                   print_r($baseCode);
                                                 }
                                                   break;
-                                                  case '7':
-                                                  if ($key[$category] == "7") {
+                                                  case '6':
+                                                  if ($key[$category] == "6") {
                                                     print_r($baseCode);
                                                   }
                                                     break;
-                                      default:
-                                      print_r($baseCode);
-                                        break;
+                                                    case '7':
+                                                    if ($key[$category] == "7") {
+                                                      print_r($baseCode);
+                                                    }
+                                                      break;
+                                        default:
+                                        print_r($baseCode);
+                                          break;
+                                      }
                                     }
                                   }
                                    ?>
