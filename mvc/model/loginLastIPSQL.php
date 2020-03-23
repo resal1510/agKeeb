8fcb1e6c7c7991913231ffb00d9db74f0e9737d7
Allan Resin <resin.allan8@gmail.com>
2020-03-23 17:51:29 +0100

    Delete old file

--- mvc/model/loginLastIPSQL.php
+++
@@ -1,7 +0,0 @@
-<?php
-$sql = "UPDATE Clients SET derniere_ip = :lastIp WHERE Clients.id_client LIKE :id";
-$stmt = $pdo->prepare($sql);
-$stmt->bindParam(":id", $idClientLogin, PDO::PARAM_STR);
-$stmt->bindParam(":lastIp", $_SERVER['REMOTE_ADDR'], PDO::PARAM_STR);
-$stmt->execute();
-?>
