7d97f9ab501f4cb9ce7274d9221791897735640f
Allan Resin <resin.allan8@gmail.com>
2020-03-23 17:53:47 +0100

    Update Product Page controller MVC

--- mvc/controller/productPageScriptStart.php
+++ mvc/controller/productPageScriptStart.php
@@ -12,18 +12,8 @@ if (isset($_GET["id_product"])) {
   }
 }
 
-require_once "config.php";
-
-$sth = $pdo->prepare("SELECT * FROM `Images` INNER JOIN Articles ON Images.article = Articles.id_article WHERE Articles.id_article LIKE :wantedId");
-$sth->bindParam(':wantedId', $idAsked, PDO::PARAM_STR);
-$sth->execute();
-$resultForImage = $sth->fetchAll(\PDO::FETCH_ASSOC);
-
-$sth2 = $pdo->prepare("SELECT * FROM `Commentaires` INNER JOIN Clients ON Commentaires.client = Clients.id_client INNER JOIN Articles ON Commentaires.article = Articles.id_article WHERE Articles.id_article LIKE :wantedId");
-$sth2->bindParam(':wantedId', $idAsked, PDO::PARAM_STR);
-$sth2->execute();
-$resultForReviews = $sth2->fetchAll(\PDO::FETCH_ASSOC);
-
+include "config.php";
+include "/var/www/allanresin2.tk/html/agkeeb/mvc/model/productStartSQL.php";
 
 //Variable for the foreach statement
 $nom = "nom_article";
