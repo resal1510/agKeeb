8ed393e6b31354de9c2bfe986ae5633f4f489ca7
Allan Resin <resin.allan8@gmail.com>
2020-05-11 13:30:12 +0200

    added user settings page

---
+++ userSettings.php
@@ -0,0 +1,14 @@
+<?php
+
+include "mvc/controller/config.php";
+include "mvc/controller/checkRememberMe.php";
+include "mvc/view/mainHeader.html";
+include "mvc/controller/navbarInclude.php";
+include 'mvc/model/paymentListAddressSQL.php';
+include "mvc/controller/userSettingsGeneral.php";
+include "mvc/controller/userSettingsCheckLogin.php";
+?>
+
+<script src="assets/js/script.min.js"></script>
+
+</html>
