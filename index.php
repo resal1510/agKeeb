afb45acb0cc9d7654612a99fb192e9c009e160d5
Allan Resin <resin.allan8@gmail.com>
2020-06-03 22:32:13 +0200

    Updates index.php
    to match with the new domain name

--- index.php
+++ index.php
@@ -6,22 +6,22 @@ include "mvc/view/mainHeader.html";
 include "mvc/controller/navbarInclude.php"
 ?>
 <p>Debug buttons (Login) :</p>
-<button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/index.php';">index</button>
-<button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/register.php';">register</button>
-<button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/login.php';">login</button>
-<button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/logout.php';">logout</button>
-<button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/oldScripts/testlogin.php';">check login</button>
+<button onclick="window.location.href = 'http://sandbox.allanresin.ch/agkeeb/index.php';">index</button>
+<button onclick="window.location.href = 'http://sandbox.allanresin.ch/agkeeb/register.php';">register</button>
+<button onclick="window.location.href = 'http://sandbox.allanresin.ch/agkeeb/login.php';">login</button>
+<button onclick="window.location.href = 'http://sandbox.allanresin.ch/agkeeb/logout.php';">logout</button>
+<button onclick="window.location.href = 'http://sandbox.allanresin.ch/agkeeb/oldScripts/testlogin.php';">check login</button>
 <div class="spacer"></div>
 <p>Debug buttons (Admin panel things) :</p>
-<button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/oldScripts/itemsManagement.php';">Manage items</button>
-<button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/oldScripts/accountsManagement.php';">Manage accounts </button>
-<button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/oldScripts/ordersManagement.php';">Manage orders </button>
+<button onclick="window.location.href = 'http://sandbox.allanresin.ch/agkeeb/oldScripts/itemsManagement.php';">Manage items</button>
+<button onclick="window.location.href = 'http://sandbox.allanresin.ch/agkeeb/oldScripts/accountsManagement.php';">Manage accounts </button>
+<button onclick="window.location.href = 'http://sandbox.allanresin.ch/agkeeb/oldScripts/ordersManagement.php';">Manage orders </button>
 <div class="spacer"></div>
 <p>Debug buttons (normal things) :</p>
-<button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/oldScripts/orderCreate.php';">Create an order</button>
-<button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/shoppingCart.php';">Shopping cart</button>
-<button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/productPage.php';">Product page</button>
-<button onclick="window.location.href = 'http://allanresin2.tk/agkeeb/catalog.php';">Catalog</button>
+<button onclick="window.location.href = 'http://sandbox.allanresin.ch/agkeeb/oldScripts/orderCreate.php';">Create an order</button>
+<button onclick="window.location.href = 'http://sandbox.allanresin.ch/agkeeb/shoppingCart.php';">Shopping cart</button>
+<button onclick="window.location.href = 'http://sandbox.allanresin.ch/agkeeb/productPage.php';">Product page</button>
+<button onclick="window.location.href = 'http://sandbox.allanresin.ch/agkeeb/catalog.php';">Catalog</button>
 </body>
 
 </html>
