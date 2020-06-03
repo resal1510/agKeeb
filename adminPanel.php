680e480d2c1bf538e1473a1da4bfcb0cbd84eb57
Allan Resin <resin.allan8@gmail.com>
2020-06-03 22:31:59 +0200

    Create adminPanel.php

---
+++ adminPanel.php
@@ -0,0 +1,14 @@
+<?php
+include "mvc/controller/config.php";
+include "mvc/controller/checkRememberMe.php";
+include "mvc/view/mainHeader.html";
+include "mvc/controller/navbarInclude.php";
+include "mvc/controller/adminPanelDataManagement.php";
+include "mvc/controller/adminPanelCheckAdmin.php";
+?>
+
+<script src="js/script.min.js"></script>
+<script src="mvc/controller/adminPanelFillModal.js"></script>
+<script src="mvc/controller/generalKeepScrollPos.js"></script>
+
+</html>
