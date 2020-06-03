910027442b506bc342a7c6ec2114ddfa22312e4d
Allan Resin <resin.allan8@gmail.com>
2020-06-03 22:34:53 +0200

    Create viewOrders.php

---
+++ mvc/view/viewOrders.php
@@ -0,0 +1,14 @@
+<main class="page faq-page">
+    <section class="dark clean-block">
+        <div class="container">
+            <div class="block-heading">
+                <h2 class="text-info" style="color: #71c3ff">Mes commandes</h2>
+            </div>
+            <div class="block-content p-4 mb-4 mx-auto" style="width:75%">
+              <?php include "mvc/controller/ordersListAll.php"; ?>
+            </div>
+        </div>
+    </section>
+</main>
+</body>
+</html>
