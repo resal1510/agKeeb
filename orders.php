615f61f9db4b89e244b07eac101c350a6c674200
Allan Resin <resin.allan8@gmail.com>
2020-06-03 22:31:54 +0200

    Create orders.php

---
+++ orders.php
@@ -0,0 +1,7 @@
+<?php
+include "mvc/controller/config.php";
+include "mvc/controller/checkRememberMe.php";
+include "mvc/view/mainHeader.html";
+include "mvc/controller/navbarInclude.php";
+include "mvc/view/viewOrders.php";
+?>
