1512204e07b97aa7089bb38bef6c4fd65285cc8e
Allan Resin <resin.allan8@gmail.com>
2020-06-11 16:24:05 +0200

    New footer and index

---
+++ mvc/view/viewIndex.php
@@ -0,0 +1,25 @@
+<style media="screen">
+html, body {
+  height: 100%;
+}
+
+html {
+  display: table;
+  margin: auto;
+}
+
+body {
+  display: table-cell;
+  vertical-align: middle;
+}
+</style>
+<body>
+    <div class="row">
+        <div class="col">
+            <h1 class="text-center text-secondary firstFade">Bienvenue sur <span style="color: rgb(113,195,255);">agKeeb</span></h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col text-center secondFade"><a class="text-secondary" href="catalog.php">Accéder au catalogue</a></div>
+    </div>
+</body>
