5c3fff853d21091bb2dc062c02cbef1fbc563f0b
Allan Resin <resin.allan8@gmail.com>
2020-06-24 12:14:38 +0200

    Create recover.php

---
+++ recover.php
@@ -0,0 +1,11 @@
+<?php
+include "mvc/controller/config.php";
+include "mvc/controller/checkRememberMe.php";
+include "mvc/controller/recoverCheckIfGet.php";
+include "mvc/view/mainHeader.html";
+include "mvc/controller/navbarInclude.php";
+include "mvc/controller/recoverMainScript.php";
+include "mvc/view/viewRecover.php";
+include "mvc/view/footer.html";
+?>
+<script src="mvc/controller/generalKeepScrollPos.js"></script>
