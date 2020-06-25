393d7146aa4af7e169800abe7697e3a0190f73be
Allan Resin <resin.allan8@gmail.com>
2020-06-25 17:28:27 +0200

    added link to new page in footer

--- mvc/view/viewIndex.php
+++ mvc/view/viewIndex.php
@@ -3,6 +3,7 @@ html {
   position: relative;
   min-height: 100%;
   display: table;
+  margin-top: -100px;
 }
 
 body {
@@ -15,44 +16,12 @@ body {
 <body>
     <div class="row">
         <div class="col">
-            <h1 class="text-center text-secondary firstFade">Bienvenue sur <span style="color: rgb(113,195,255);">agKeeb</span></h1>
+            <h1 class="text-center text-secondary firstFade pulsate-bck">Bienvenue sur <span style="color: rgb(113,195,255);">agKeeb</span></h1>
         </div>
     </div>
     <div class="row">
         <div class="col text-center secondFade"><a class="text-secondary" href="catalog.php">Accéder au catalogue</a></div>
     </div>
     <div class="footer-dark fixed-bottom">
-        <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6 col-md-2 item">
-                        <h3>À propos</h3>
-                        <ul>
-                            <li><a href="#">L'équipe</a></li>
-                            <li><a href="#">Notre histoire</a></li>
-                            <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Travailler chez agKeeb</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-6 col-md-3 item">
-                        <h3>Contact</h3>
-                        <ul>
-                            <li><a href="mailto:gaetan.banderet@gmail.com">gaetan.banderet@gmail.com</a></li>
-                            <li><a href="mailto:resin.allan8@gmail.com">resin.allan8@gmail.com</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-6 col-md-2 item">
-                        <h3>Liens utiles</h3>
-                        <ul>
-                            <li><a href="rss.xml">Flux RSS</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-5 item text">
-                        <h3>agKeeb</h3>
-                        <p>Souvent considéré comme étant le meilleur site d'e-commerce de Suisse, agKeeb a réussi à percer en proposant des articles de qualité à prix raisonnables.</p>
-                    </div>
-                </div>
-                <p class="copyright">agKeeb © 2020</p>
-            </div>
-        </footer>
     </div>
 </body>
