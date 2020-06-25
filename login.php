bf64a6015466e45ce3a3cf033e50864f25620006
Allan Resin <resin.allan8@gmail.com>
2020-06-25 17:26:03 +0200

    fix

--- login.php
+++ login.php
@@ -3,5 +3,5 @@ include "mvc/controller/loginPageScript.php";
 include "mvc/view/mainHeader.html";
 include "mvc/controller/navbarInclude.php";
 include "mvc/view/viewLogin.php";
-include "mvc/view/footer.html"
+include "mvc/view/footer.html";
 ?>
