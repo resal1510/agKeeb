2e8db63b5b0a64746edc3edce4023ed60f3ce73d
Allan Resin <resin.allan8@gmail.com>
2020-06-25 17:25:56 +0200

    new about page

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php
+include "mvc/controller/config.php";
+include "mvc/controller/checkRememberMe.php";
+include "mvc/view/mainHeader.html";
+include "mvc/controller/navbarInclude.php";
+include "mvc/view/viewAbout.php";
+include "mvc/view/footer.html";
+?>
---
+++ mvc/view/viewAbout.php
@@ -0,0 +1,14 @@
+<body style="height:100%">
+  <div class="container" style="margin-bottom:auto">
+  <div class="row">
+    <div class="col">
+      <h3>A propos d'agKeeb</h3><br>
+      <p>Depuis longtemps, l'idée de faire un shop en ligne de pièces détachées de claviers mécaniques nous était venue.</p>
+      <p>Puis un jour, pour les cours, un projet nous tomba dans les mains : Faire un shop en ligne de A-z. C'était l'occasion idéale !</p>
+      <br>
+      <p>Du coup, nous nous sommes associés pour faire ce projet ensemble, et de mettre à bien, notre superbe idée de faire un shop de claviers. Le nom "agKeeb" vient simplement de nos noms a(Allan), g(Gaétan), et "keeb", qui rappelle le mot anglais "Keyboard", se traduisant "Clavier" en bon français</p>
+      <p>Maintenant, nous avonc créé <b>LE</b> site de référence, suisse, de clavier mécanique ! incroyable non ?</p>
+    </div>
+  </div>
+</div>
+</body>
