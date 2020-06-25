628fd605955620579a96a818353e94330f7096b6
Allan Resin <resin.allan8@gmail.com>
2020-06-25 17:25:44 +0200

    New team page

---
+++ mvc/view/viewTeam.php
@@ -0,0 +1,36 @@
+<link href="https://emoji-css.afeld.me/emoji.css" rel="stylesheet">
+<body style="overflow-x:hidden">
+  <div class="card-deck" style="margin-bottom:50px">
+    <div class="container">
+    <div class="row justify-content-md-center">
+      <div class="col col-lg-5">
+        <div class="card">
+          <img src="uploads/allan2.jpeg" class="card-img-top" alt="..." style="height:500px;object-fit: cover;">
+          <div class="card-body">
+            <h5 class="card-title">Allan Resin</h5>
+            <p class="card-text">Développeur principalement backend, s'est occupé de toute la logique d'agKeeb. C'est grâce à lui que vous pouvez par exemple, vous connecter et passer commande  <i class="em-svg em-sunglasses" aria-role="presentation" aria-label="SMILING FACE WITH SUNGLASSES"></i></p>
+          </div>
+          <div class="card-footer">
+            <a href="https://github.com/resal1510" style="color:black"><i class="fab fa-github"></i></a>
+            <a href="https://allanresin.ch/" style="color:black"><i class="fa fa-globe"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-auto">
+      </div>
+      <div class="col col-lg-5">
+        <div class="card">
+          <img src="uploads/gaetan.jpeg" class="card-img-top" alt="..." style="height:500px;object-fit: cover;">
+          <div class="card-body">
+            <h5 class="card-title">Gaétan Banderet</h5>
+            <p class="card-text">Développeur principalement frontend, s'est occupé de tout le visuel du shop. C'est grâce à lui qu'agKeeb est d'une beauté resplendissante, de jour comme de nuit  <i class="em-svg em-fire" aria-role="presentation" aria-label="FIRE"></i></p>
+          </div>
+          <div class="card-footer">
+            <a href="https://github.com/ayygurl" style="color:black"><i class="fab fa-github"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
