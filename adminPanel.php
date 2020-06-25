a308183210fe2aa4a125e2f692d4fbff73eea993
Allan Resin <resin.allan8@gmail.com>
2020-06-25 17:26:18 +0200

    temp modification

--- adminPanel.php
+++ adminPanel.php
@@ -6,9 +6,11 @@ include "mvc/view/mainHeader.html";
 <script src="/agkeeb/mvc/controller/adminPanelFillModal.js"></script>
 <script src="/agkeeb/mvc/controller/generalKeepScrollPos.js"></script>
 <?php
+
+include 'mvc/model/paymentListAddressSQL.php';
 include "mvc/controller/navbarInclude.php";
-include "mvc/controller/adminPanelDataManagement.php";
 include "mvc/controller/adminPanelCheckAdmin.php";
+include "mvc/controller/adminPanelDataManagement.php";
 include "mvc/view/footer.html";
 ?>
 </html>
